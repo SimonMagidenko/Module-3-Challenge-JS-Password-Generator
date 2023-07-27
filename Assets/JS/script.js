4c1329040ab8c967b8bde169dc729430485540b5
SimonM <smagidenko@gmail.com>
2023-07-27 19:31:46 -0400

    Disabling Prettier

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,16 +1,49 @@
 // Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
+let lowerCaseAlphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+function generatePassword() {
+  if (confirm("Do you want your password to contain lowercase letters?"))
+    // Build a string based off of randomly selected values from an array
+
+    return "Random Password";
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
